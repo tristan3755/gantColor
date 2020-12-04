d3bc7889a9244577820d401daa09d23989c532d9
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-12-04 16:37:52 +0100

    color

--- email.php
+++ email.php
@@ -2,7 +2,6 @@
 
 var_dump($_POST);
 
-
 $mess = ''.$_POST['prenom'].' '.$_POST['nom'].' '.$_POST['objet'].' '.$_POST['mail'].'from :'. $_POST['email'];
 
 $mail = mail('tristan3755@gmail.com','Envoi depuis la page Contact',$mess);
