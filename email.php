89509739ad9b756317131d90b32df15c4281e1c7
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-12-15 14:14:39 +0100

    color

--- email.php
+++ email.php
@@ -3,11 +3,13 @@
 if(!empty($_POST)){
 var_dump($_POST);
 
-$mess = ''.$_POST['prenom'].' '.$_POST['nom'].' '.$_POST['objet'].' '.$_POST['mail'].'from :'. $_POST['email'];
+$mess = ''.$_POST['prenom'].' '.$_POST['nom'].' , '.$_POST['objet'].' : '.$_POST['mail'].'from :'. $_POST['email'];
 
 $mail = mail('tristan3755@gmail.com','Envoi depuis la page Contact',$mess);
 
-}else{
-    echo "veuillez remplir tous les champs";
 }
+
+header('Location:./index.html');
+exit;
+
 ?>
