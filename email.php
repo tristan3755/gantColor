2fa58d910f2160d5a314280229f165780ba328ab
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-12-17 16:13:45 +0100

    color

--- email.php
+++ email.php
@@ -5,7 +5,7 @@ if(!empty($_POST)){
 
 $mess = ''.$_POST['prenom'].' '.$_POST['nom'].' , '.$_POST['objet'].' : '.$_POST['mail'].'     '.'from :'. $_POST['email'];
 
-$mail = mail('tristan3755@gmail.com','Envoi depuis la page Contactdu site gantColor',$mess);
+$mail = mail('tristan3755@gmail.com','Envoi depuis la page Contact du site gantColor',$mess);
 
 }
 
